fdc34ad0d1ea399644caf13465e8a05bd234b580
George Zinyakov <gzinyakov@alfabank.ru>
2019-09-15 18:48:29 +0300

    feature: added project sample

---
+++ src/main/java/org/ziniakov/musiccalendar/MusiccalendarApplication.java
@@ -0,0 +1,13 @@
+package org.ziniakov.musiccalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiccalendarApplication.class, args);
+	}
+
+}
